1216ab9dc99d7e6a9b242161dc9a3a5c0d17c543
voidful <voidful@users.noreply.github.com>
2020-09-29 16:48:46 +0000

    deploy: 206fd254e5a644117d689deb7ce81ac481132da8

---
+++ precache-manifest.b65511e6a7063501b2399359b853935d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b17058c8c2a4473a4513a1cf6504a22",
+    "url": "/voidful/DG-Showcase/index.html"
+  },
+  {
+    "revision": "d3d9c8416405da19e847",
+    "url": "/voidful/DG-Showcase/static/css/main.736bfa08.chunk.css"
+  },
+  {
+    "revision": "927a38ab0b644a0886d6",
+    "url": "/voidful/DG-Showcase/static/js/2.d947e8c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/voidful/DG-Showcase/static/js/2.d947e8c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3d9c8416405da19e847",
+    "url": "/voidful/DG-Showcase/static/js/main.4952149f.chunk.js"
+  },
+  {
+    "revision": "c23c114da2415fe81896",
+    "url": "/voidful/DG-Showcase/static/js/runtime-main.4bf1f7fe.js"
+  }
+]);
