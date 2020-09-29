206fd254e5a644117d689deb7ce81ac481132da8
voidful <voidful.stack@gmail.com>
2020-09-30 00:45:02 +0800

    fix(build-error): Expected '===' and instead saw '=='  eqeqeq

--- src/component/main.js
+++ src/component/main.js
@@ -17,7 +17,7 @@ class MAIN extends React.Component {
         var vars = query.split("&");
         for (var i = 0; i < vars.length; i++) {
             var pair = vars[i].split("=");
-            if (pair[0] == variable) {
+            if (pair[0] === variable) {
                 return pair[1];
             }
         }
